07eec8723cb102e1992a2f62c6f01836502ee707
Rubén Valls <96390939+RubenValls@users.noreply.github.com>
2022-10-10 21:11:49 +0200

    Add files via upload

---
+++ 04-uso_Numbers_Javascript/numbers_Use_JavaScript.js
@@ -0,0 +1,7 @@
+var altura = 188;
+var alturaMetros = 1.88;
+var peso = 86.8;
+var alturaRedondeada = alturaMetros.toFixed(1);
+var pesoRedondeado = Math.floor(peso);
+var comprobacion;
+console.log(Number.MAX_VALUE+1 === Number.MAX_VALUE) //Valor máximo +1 es igual al valor máximo
