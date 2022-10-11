df7a15460d8da8b61421a596009c77d35b4454b7
Rubén Valls <96390939+RubenValls@users.noreply.github.com>
2022-10-11 13:10:24 +0200

    Add files via upload

---
+++ 06-sets_Objetos_JavaScript/set_Basics.js
@@ -0,0 +1,9 @@
+let arrawNombres = ["Pedro", "Juan", "Maria", "Rubén", "Esteban", "Pedro", "Lucia", "Esteban"];
+let conjunto = new Set(arrawNombres);
+console.log(conjunto);
+
+conjunto.add("Rubén");
+console.log(conjunto);
+
+conjunto.add("JavaScript");
+console.log(conjunto);
