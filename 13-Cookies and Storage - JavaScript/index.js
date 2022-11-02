81c83e78d5b2557db7b5c6fa37400b629465c495
Rubén Valls <96390939+RubenValls@users.noreply.github.com>
2022-11-02 12:42:38 +0100

    Add files via upload

---
+++ 13-Cookies and Storage - JavaScript/index.js
@@ -0,0 +1,12 @@
+let fname = "Ruben"
+let surname = "Valls"
+
+let person = {
+    firstName : fname,
+    secondName: surname
+}
+
+//sessionStorage.setItem("person", person)
+//localStorage.setItem("person", person)
+
+//document.cookie = "person="+person+"; expires=Thu, 18 Dec 2023 12:00:00 UTC"
